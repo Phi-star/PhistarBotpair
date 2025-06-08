dedf1ae25b03e81f1ff0c8bcb5eebc6e22c46ac8
Phi-star <saliuphilip8@gmail.com>
2025-06-08 08:38:38 +0100

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+// components/Footer.js
+export default function Footer() {
+    return (
+        <footer className="bg-gray-800 text-white py-4 mt-10">
+            <div className="container mx-auto text-center">
+                <p>© 2025 Phistar Bot Inc. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+}
